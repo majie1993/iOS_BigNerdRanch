f827c32b1bfd86df635c806bdae290b26695f7a6
WaterWood <majieqq@gmail.com>
2014-05-19 10:45:58 +0800

    Finish chapter 13

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -9,10 +9,14 @@
 #import "BNRDrawView.h"
 #import "BNRLine.h"
 
-@interface BNRDrawView()
+@interface BNRDrawView() <UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
+@property (nonatomic, weak) BNRLine *selectedLine;
+@property (nonatomic, strong) UIColor *lineColor;
+
+@property (nonatomic, strong) UIPanGestureRecognizer *moveRecognizer;
 
 @end
 
@@ -26,6 +30,32 @@
         self.backgroundColor = [UIColor grayColor];
         self.linesInProgress = [[NSMutableDictionary alloc] init];
         self.multipleTouchEnabled = YES;
+        
+        self.lineColor = [UIColor blackColor];
+        
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
+        
+        UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipe:)];
+        swipeRecognizer.numberOfTouchesRequired = 3;
+        swipeRecognizer.delaysTouchesBegan = YES;
+        swipeRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+        [self addGestureRecognizer:swipeRecognizer];
+        
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
+        
+        UILongPressGestureRecognizer *pressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
+        [self addGestureRecognizer:pressRecognizer];
+        
+        self.moveRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveLine:)];
+        self.moveRecognizer.delegate = self;
+        self.moveRecognizer.cancelsTouchesInView = NO;
+        [self addGestureRecognizer:self.moveRecognizer];
     }
     
     
@@ -45,7 +75,7 @@
 
 - (void)drawRect:(CGRect)rect
 {
-    [[UIColor blackColor] set];
+    [self.lineColor set];
     
     for (BNRLine *line in self.finishedLines) {
         [self strokeLine:line];
@@ -55,11 +85,20 @@
     for (NSValue *key in self.linesInProgress) {
         [self strokeLine:self.linesInProgress[key]];
     }
+    
+    if (self.selectedLine) {
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
+    }
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    if ([UIMenuController sharedMenuController].isMenuVisible) {
+        return;
+    }
     
     for (UITouch *t in touches) {
         CGPoint location = [t locationInView:self];
@@ -72,13 +111,16 @@
         self.linesInProgress[key] = line;
     }
     
-    
     [self setNeedsDisplay];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    if ([UIMenuController sharedMenuController].isMenuVisible) {
+        return;
+    }
     
     for (UITouch *t in touches) {
         NSValue *key = [NSValue valueWithNonretainedObject:t];
@@ -92,6 +134,10 @@
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    if ([UIMenuController sharedMenuController].isMenuVisible) {
+        return;
+    }
     
     for (UITouch *t in touches) {
         NSValue *key = [NSValue valueWithNonretainedObject:t];
@@ -113,6 +159,150 @@
         [self.linesInProgress removeObjectForKey:key];
     }
     [self setNeedsDisplay];
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    if (gestureRecognizer == self.moveRecognizer) {
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (void)doubleTap: (UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Double Tap");
+    
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    
+    [self setNeedsDisplay];
+}
+
+- (void)tap: (UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    
+    if (self.selectedLine) {
+        [self becomeFirstResponder];
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        UIMenuItem *deleteItem = [[UIMenuItem alloc] initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        menu.menuItems = @[deleteItem];
+        
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    } else {
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)longPress: (UIGestureRecognizer *)gr
+{
+    if (gr.state == UIGestureRecognizerStateBegan) {
+        CGPoint point = [gr locationInView:self];
+        self.selectedLine = [self lineAtPoint:point];
+        
+        if (self.selectedLine) {
+            [self.linesInProgress removeAllObjects];
+        }
+    } else if (gr.state == UIGestureRecognizerStateEnded) {
+        self.selectedLine = nil;
+    }
+    [self setNeedsDisplay];
+}
+
+- (void)swipe:(UISwipeGestureRecognizer *)gr
+{
+    NSLog(@"swipe");
+    
+    [self becomeFirstResponder];
+    
+    UIMenuController *menu = [UIMenuController sharedMenuController];
+    UIMenuItem *blueItem = [[UIMenuItem alloc] initWithTitle:@"blue" action:@selector(changeBlueColor:)];
+    UIMenuItem *blackItem = [[UIMenuItem alloc] initWithTitle:@"black" action:@selector(changeBlackColor:)];
+    UIMenuItem *purpleItem = [[UIMenuItem alloc] initWithTitle:@"purple" action:@selector(changePurpleColor:)];
+    menu.menuItems = @[blueItem, blackItem, purpleItem];
+    
+    [menu setTargetRect:CGRectMake(10, 10, 2, 2) inView:self];
+    [menu setMenuVisible:YES animated:YES];
+}
+
+- (void)changeBlueColor: (id)sender
+{
+    self.lineColor = [UIColor blueColor];
+}
+- (void)changeBlackColor: (id)sender
+{
+    self.lineColor = [UIColor blackColor];
+}
+- (void)changePurpleColor: (id)sender
+{
+    self.lineColor = [UIColor purpleColor];
+}
+
+- (void)moveLine:(UIPanGestureRecognizer *)gr
+{
+    // if menu visible, don't move the selected line
+    if (!self.selectedLine || [UIMenuController sharedMenuController].isMenuVisible) {
+        return;
+    }
+    
+    if (gr.state == UIGestureRecognizerStateChanged) {
+        CGPoint translation = [gr translationInView:self];
+        
+        CGPoint begin = self.selectedLine.begin;
+        CGPoint end = self.selectedLine.end;
+        begin.x += translation.x;
+        begin.y += translation.y;
+        end.x += translation.x;
+        end.y += translation.y;
+        
+        self.selectedLine.begin = begin;
+        self.selectedLine.end = end;
+        [self setNeedsDisplay];
+        [gr setTranslation:CGPointZero inView:self];
+        
+        NSLog(@"velocity: %f %f", [gr velocityInView:self].x, [gr velocityInView:self].y);
+    }
+}
+
+- (void)deleteLine: (id)sender
+{
+    [self.finishedLines removeObject:self.selectedLine];
+    [self setNeedsDisplay];
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+#pragma mark - utils
+
+- (BNRLine *)lineAtPoint:(CGPoint)p
+{
+    // Find a line close to p
+    for (BNRLine *l in self.finishedLines) {
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        
+        for (float t=0.0; t<=1.0; t+=0.05) {
+            float x = start.x + t*(end.x - start.x);
+            float y = start.y + t*(end.y - start.y);
+            if (hypot(x - p.x, y - p.y) < 20.0) {
+                return l;
+            }
+        }
+        
+    }
+    
+    return nil;
 }
 
 @end
