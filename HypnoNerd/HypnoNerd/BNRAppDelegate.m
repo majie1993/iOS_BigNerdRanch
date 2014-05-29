22eb038527942a65b8c617536a3e38b5b9b079e1
WaterWood <majieqq@gmail.com>
2014-05-29 21:45:45 +0800

    Finish chapter 27

--- HypnoNerd/HypnoNerd/BNRAppDelegate.m
+++ HypnoNerd/HypnoNerd/BNRAppDelegate.m
@@ -72,12 +72,14 @@
 
 - (BOOL)application:(UIApplication *)application shouldSaveApplicationState:(NSCoder *)coder
 {
-    return YES;
+    //return YES;
+    return NO;
 }
 
 - (BOOL)application:(UIApplication *)application shouldRestoreApplicationState:(NSCoder *)coder
 {
-    return YES;
+    //return YES;
+    return NO;
 }
 
 @end
--- HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
@@ -10,6 +10,8 @@
 
 @interface BNRHypnosisViewController() <UITextFieldDelegate>
 
+@property (nonatomic, weak) UITextField *textField;
+
 @end
 
 @implementation BNRHypnosisViewController
@@ -19,8 +21,9 @@
     CGRect frame = [UIScreen mainScreen].bounds;
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
     
-    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
-    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(40, -20, 240, 30)];
+    self.textField = textField;
+    
     textField.borderStyle = UITextBorderStyleRoundedRect;
     textField.placeholder = @"Hypnotize me";
     textField.returnKeyType = UIReturnKeyDone;
@@ -55,6 +58,24 @@
     NSLog(@"BNRHypnosisViewController loaded its view");
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    NSLog(@"View did appear");
+    
+    [UIView animateWithDuration:2.0
+                          delay:0.0
+         usingSpringWithDamping:0.25
+          initialSpringVelocity:0.0
+                        options:0
+                     animations:^{
+                         CGRect frame = CGRectMake(40, 70, 240, 30);
+                         self.textField.frame = frame;
+                     }
+                     completion:NULL];
+}
+
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [self drawHypnoticMessage:textField.text];
@@ -86,6 +107,34 @@
         messageLabel.frame = frame;
         
         [self.view addSubview:messageLabel];
+        
+        messageLabel.alpha = 0.0;
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseIn
+                         animations:^{
+                             messageLabel.alpha = 1.0;
+                         } completion:NULL];
+        
+        [UIView animateKeyframesWithDuration:1.0
+                                       delay:0.0
+                                     options:0
+                                  animations:^{
+                                      [UIView addKeyframeWithRelativeStartTime:0
+                                                              relativeDuration:0.8
+                                                                    animations:^{
+                                                                        messageLabel.center = self.view.center;
+                                                                    }];
+                                      [UIView addKeyframeWithRelativeStartTime:0.8
+                                                              relativeDuration:0.2
+                                                                    animations:^{
+                                                                        int x = arc4random() % width;
+                                                                        int y = arc4random() % height;
+                                                                        messageLabel.center = CGPointMake(x, y);
+                                                                    }];
+                                  } completion:^(BOOL finished){
+                                      NSLog(@"Animation finished");
+                                  }];
         
         UIInterpolatingMotionEffect *motionEffect;
         motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
